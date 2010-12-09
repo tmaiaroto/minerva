f42b87f36d935626cba5441b46918b446162529a
tom_m <tom@shift8creative.com>
2010-12-09 09:08:06 -0800

    100% coverage on Rules adapter

--- libraries/li3_access/extensions/adapter/security/access/Rules.php
+++ libraries/li3_access/extensions/adapter/security/access/Rules.php
@@ -9,6 +9,7 @@ class Rules extends \lithium\core\Object {
     // Rules are closures that must return true or false
     protected static $_rules = array();
     
+    // Set some default rules to use
     public static function __init() {
 	self::$_rules = array(
 	    'allowAll' => function() {
@@ -18,10 +19,7 @@ class Rules extends \lithium\core\Object {
 		return false;  
 	    },
 	    'allowAnyUser' => function($user) {
-		if(!empty($user)) {
-		    return true;
-		}
-		return false;
+		return (!empty($user)) ? true:false;
 	    },
 	    'allowIp' => function($user, $request, $options) {
 		$options += array('ip' => false);
@@ -30,19 +28,8 @@ class Rules extends \lithium\core\Object {
 	);
     }
     
-    // Set defaults if left out of the configuration
-    protected static function _initConfig($name, $config) {
-        $defaults = array(
-            'message' => 'You are not permitted to access this area.',
-            'login_redirect' => '/'
-        );
-        
-        $config = parent::_initConfig($name, $config) + $defaults;
-        return $config;
-    }
-    
     /**
-     * The Rules adapter will use check to test the provided data
+     * The `Rules` adapter will use check to test the provided data
      * against a number of given rules. Extra data that may be required
      * to make an informed decision about access can be passed in the
      * $options array. This extra data will vary from app to app and rules
@@ -58,16 +45,11 @@ class Rules extends \lithium\core\Object {
     public function check($user, $request, array $options = array()) {
 	$options += array('rules' => array());
 	if(empty($options['rules'])) {
-	   return array(); 
+	   return array('rule' => false, 'message' => $options['message'], 'redirect' => $options['redirect']); 
 	}
 	
 	// If a single rule was passed, wrap it in an array so it can be iterated as if there were multiple
-	$rules = $options['rules'];
-	if(isset($rules['rules']['rule'])) {
-	    $rules = array(
-		$rules
-	    );
-	}
+	$rules = (isset($options['rules']['rule'])) ? array($options['rules']):$options['rules'];
 	
 	$access_response = array();
 	
@@ -76,6 +58,7 @@ class Rules extends \lithium\core\Object {
 	    // make sure the rule is set and is a string to check for a closure to call or a closure itself
 	    if((isset($rule['rule'])) && ((is_string($rule['rule'])) || (is_callable($rule['rule'])))) {
 		
+		$rule_result = false;
 		// The added rule closure will be passed the user data
 		if(in_array($rule['rule'], array_keys(self::$_rules))) {
 		    // The rule closure will be passed the user, request and the rule array itself which could contain extra data required by the specific rule.
@@ -83,9 +66,6 @@ class Rules extends \lithium\core\Object {
 		} elseif(is_callable($rule['rule'])) {
 		    // The rule can be defined as a closure on the fly, no need to call add()
 		    $rule_result = call_user_func($rule['rule'], $user, $request, $rule);
-		} else {
-		    // In case something went wrong, deny access
-		    $rule_result = false;
 		}
 		
 		if($rule_result === false) {
@@ -101,18 +81,36 @@ class Rules extends \lithium\core\Object {
 	return $access_response;
     }
 
-    /*
+    /**
      * Adds an Access rule. This works much like the Validator class.
      * All rules should be anonymous functions and will be passed
      * $user, $request, and $options which will contain the entire
      * rule array which contains its own name plus other data that
      * could be used to determine access.
+     *
+     * @param string $name The rule name.
+     * @param function $rule The closure for the rule, which has to return true or false.
     */
     public static function add($name, $rule = null) {
         if (!is_array($name)) {
             $name = array($name => $rule);
         }
         self::$_rules = Set::merge(self::$_rules, $name);
+    }
+    
+    /**
+     * Simply returns the rules that are currently available.
+     * Optionally, passing a name will return just that rule
+     * or false if it doesn't exist.
+     *
+     * @param string $name The rule name (optional).
+     * @return mixed Either an array of rule closures, a single rule closure, or false.
+    */
+    public function getRules($name = false) {
+	if($name) {
+	    return (isset(self::$_rules[$name])) ? self::$_rules[$name]:false;
+	}
+	return self::$_rules;
     }
     
 }
--- libraries/li3_access/tests/cases/extensions/adapter/security/access/RulesTest.php
+++ libraries/li3_access/tests/cases/extensions/adapter/security/access/RulesTest.php
@@ -20,10 +20,6 @@ class RulesTest extends \lithium\test\Unit {
 	        'adapter' => 'Rules'
 	    )
 	));
-	
-	Access::adapter('test_rulebased')->add('testDeny', function($user, $request, $options) {
-	    return false;
-	});
     }
     
     public function tearDown() {}
@@ -34,13 +30,22 @@ class RulesTest extends \lithium\test\Unit {
 	// Multiple rules, they should all pass
         $rules = array(
             array('rule' => 'allowAnyUser', 'message' => 'You must be logged in.'),
+	    array('rule' => 'allowAll', 'message' => 'You must be logged in.'),
             array('rule' => 'allowIp', 'message' => 'You can not access this from your location. (IP: ' . $_SERVER['REMOTE_ADDR'] . ')', 'ip' => $_SERVER['REMOTE_ADDR'])
         );
         $expected = array();
         $result = Access::check('test_rulebased', array('username' => 'Tom'), $request, array('rules' => $rules));
         $this->assertEqual($expected, $result);
 	
-	// Single rule, but it should fail because user is an empty array
+	// Single rule in multi-demnsional array
+	$rules = array(
+            array('rule' => 'denyAll', 'message' => 'You must be logged in.')
+        );
+        $expected = array('rule' => 'denyAll', 'message' => 'You must be logged in.', 'redirect' => '/');
+        $result = Access::check('test_rulebased', array('username' => 'Tom'), $request, array('rules' => $rules));
+        $this->assertEqual($expected, $result);
+	
+	// Single rule (single array), but it should fail because user is an empty array
 	$rules = array('rule' => 'allowAnyUser', 'message' => 'You must be logged in.');
         $expected = array('message' => 'You are not permitted to access this area.', 'redirect' => '/');
         $result = Access::check('test_rulebased', array(), $request, array('rules' => $rules));
@@ -48,15 +53,12 @@ class RulesTest extends \lithium\test\Unit {
 	// and if false instead of an empty array (because one might typically run Auth:check() which could return false)
 	$result = Access::check('test_rulebased', false, $request, array('rules' => $rules));
         $this->assertEqual($expected, $result);
-    
-	// The add() method to add a rule
-	$rules = array(
-	    array('rule' => 'testDeny', 'message' => 'Access denied.')
-        );
-	$expected = array('rule' => 'testDeny', 'message' => 'Access denied.', 'redirect' => '/');
-	$result = Access::check('test_rulebased', array('username' => 'Tom'), $request, array('rules' => $rules));
-	$this->assertEqual($expected, $result);
 	
+	// No rules
+	$expected = array('rule' => false, 'message' => 'You are not permitted to access this area.', 'redirect' => '/');
+        $result = Access::check('test_rulebased', array('username' => 'Tom'), $request);
+        $this->assertEqual($expected, $result);
+    
 	// Adding a rule "on the fly" by passing a closure, this rule should pass
 	$rules = array(
 	    array('rule' => function($user, $request, $options) { return $user['username'] == 'Tom'; }, 'message' => 'Access denied.')
@@ -64,7 +66,27 @@ class RulesTest extends \lithium\test\Unit {
 	$expected = array();
 	$result = Access::check('test_rulebased', array('username' => 'Tom'), $request, array('rules' => $rules));
 	$this->assertEqual($expected, $result);
+    }
+    
+    public function testAdd() {
+	$request = new Request();
 	
+	// The add() method to add a rule
+	Access::adapter('test_rulebased')->add('testDeny', function($user, $request, $options) {
+	    return false;
+	});
+	
+	$rules = array(
+	    array('rule' => 'testDeny', 'message' => 'Access denied.')
+        );
+	$expected = array('rule' => 'testDeny', 'message' => 'Access denied.', 'redirect' => '/');
+	$result = Access::check('test_rulebased', array('username' => 'Tom'), $request, array('rules' => $rules));
+	$this->assertEqual($expected, $result);
+	
+	// Make sure the rule got added to the $_rules property
+	$this->assertTrue(is_callable(Access::adapter('test_rulebased')->getRules('testDeny')));
+	
+	$this->assertTrue(is_array(Access::adapter('test_rulebased')->getRules()));
     }
     
 }    
