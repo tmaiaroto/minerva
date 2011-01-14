2a8702216de0558de53626e5c280df3b23b29e05
tom_m <tom@shift8creative.com>
2011-01-14 10:08:27 -0800

    shuffling around templates. adding little "getting started" static page

---
+++ views/menus/static/main_administration.html.php
@@ -0,0 +1,38 @@
+<?php
+use lithium\security\Auth;
+if(Auth::check('minerva_user')) { ?>
+<ul id="main_administration_menu" class="nav main">
+    <li class="menu_first"><a href="/admin">Dashboard</a></li>
+    <li>
+        <a href="/pages">Pages</a>
+        <ul>
+            <li><a href="/pages">List All</a></li>
+            <li><a href="/pages/create">Create New</a></li>
+        </ul>
+    </li>
+    <li>
+        <a href="/blocks">Blocks</a>
+        <ul>
+            <li><a href="/blocks">List All</a></li>
+            <li><a href="/blocks/create">Create New</a></li>
+        </ul>
+    </li>
+    <li>
+        <a href="/users">Users</a>
+        <ul>
+            <li><a href="/users">List All</a></li>
+            <li><a href="/users/create">Create New</a></li>
+        </ul>
+    </li>
+    <li>
+        <a href="/">System</a>
+        <ul>
+            <li><a href="/admin/system_status">System Status</a></li>
+            <li><a href="/test" target="_blank">Unit Test Dashboard</a></li>
+        </ul>
+    </li>
+    <li class="menu_last">
+        <a href="/users/logout">Logout</a>
+    </li>
+</ul>
+<?php } ?>
