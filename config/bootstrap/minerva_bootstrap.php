d659bf03a120faf627734baf5ac4a7cb86a019e6
tom_m <tom@shift8creative.com>
2011-04-08 23:39:34 -0700

    cleaning up minerva_bootstrap some more

--- config/bootstrap/minerva_bootstrap.php
+++ config/bootstrap/minerva_bootstrap.php
@@ -4,18 +4,22 @@
  * Basically, setting all the possible paths for templates.
  *
  * Routes are going to help us a lot because there's certain files we don't want to touch in order to change
- * the templates; for example, core Minerva files. It would create issues for updates. So our routes, which are
- * custom for each site, will be able to change up some render paths for us.
+ * the templates; for example, core Minerva files. It would create issues for updates. So new routes, from
+ * plugins that use minerva, will be able to change up some render paths.
  *
- * Also, we may want to use the admin interface from Minerva and rather than duplicate the template, we can
- * simply use it in our new add on libraries so that if it ever changes, there wouldn't be any dated templates.
+ * Also, we may want to use the admin interface from Minerva and rather than duplicate the template, so we can
+ * simply use it in our new add on plugins so that if it ever changes, there wouldn't be any dated templates.
+ * Of course, provided the plugin's needs are met by the default admin templates. Otherwise, new templates.
  *
- * Lithium allows us to pass an array of template paths to render. It will use the first available template.
+ * Lithium allows us to pass an array of template paths to render. It will use the first found template.
  * So we have a graceful fallback system if a template isn't found in one location.
- *
- * This is applied to the Dispatcher::_callable() and done early so other filters can be applied without
- * conflict. So if a 3rd party library wanted to apply a filter to Media::render() for example, it could,
- * but it would be after this.
+ * It will go all the way back to loading a missing template and layout file in fact.
+ * That doesn't mean "404" pages and it doesn't mean it's not possible to see a white page still,
+ * but it helps.
+ * 
+ * All this is applied to the Dispatcher::_callable() so it happens really early on in the dispatcher process.
+ * This allows other plugins to apply their filters aftward; keep in mind the order in which libraries are
+ * added and if the library relies on Minerva, add it after.
 */
 
 use lithium\action\Dispatcher;
@@ -26,67 +30,57 @@ use lithium\core\Libraries;
 use lithium\template\View;
 use \Exception;
 
-Dispatcher::applyFilter('run', function($self, $params, $chain) {
-	// TODO: MOVE THIS TO _call FILTER
-	
-	// Dependency checks for the CMS
-	$library_deps = array(
-		'li3_flash_message'
-	);
-	
-	$missing_deps = array();
-	foreach($library_deps as $library) {
-		if(Libraries::get($library) == null) {
-			$missing_deps[] = $library;
-		//	throw new Exception('Hey! You don\'t have that library!');
-		}
-	}
-	
-	if(!empty($missing_deps)) {
-		$view = new View(array('loader' => 'Simple', 'renderer' => 'Simple'));
-		$message =  'You are missing the following libraries that Minerva depends on:<br />';
-		foreach($missing_deps as $library) {
-			$message .= $library . '<br />';
-		}
-		$message .= '<br /><br />You will need to ensure that you have these libraries yourself for now, but in the future there will hopefully be an option to automatically download and install them.';
-		
-		echo $view->render(array('element' => $message));
-		exit();
-	}
-	
-	return $chain->next($self, $params, $chain);
-});
-
 Dispatcher::applyFilter('_callable', function($self, $params, $chain) {
     
-    /*
-     * So the problem with redirects and building requests is that since the "app" folder was changed to "minerva"
-     * the "_base" property is not set properly. In the Request class there's a method called base() that sets it.
-     * It basically does a string replace on "app/webroot" ... But we have "minerva/webroot" So we can change the
-     * /minerva/webroot/index.php file and pass in an empty base key of "" to fix the issue.
-     * I would rather set it here in the filter since that's where all the major changes are taking place.
-     * I'd like to limit changes to a specific area to avoid complexity...But _base is protected as well as base().
-     * So we can't set it here. It can only be set by instantiation.
-     * Alternatively we can write a new class (extending Request) and use that instead...
-     * TODO: Look into that and in general a sub dispatcher that might avoid several issues and clean up this code.
-     *
-     * For now the index.php file has been changed, but that may cause problems elsewhere. Not sure yet.
-     * Now all the redirects don't show a URL of site.com/minerva/blog it will be the expected site.com/blog
-     * Both work though.
-    */
+	// Only apply the following when using the minerva library
 	if(isset($params['request']->params['library']) && $params['request']->params['library'] == 'minerva') {
-		
-		
 		// Pass through a few Minerva configuration variables
 		$config = Libraries::get('minerva');
 		$params['minerva_base'] = isset($config['base_url']) ? $config['base_url'] : '/minerva';
 		$params['minerva_admin_prefix'] = isset($config['admin_prefix']) ? $config['admin_prefix'] : 'admin';
+		// some default controllers that utilize static view templates using a "view" action
+		$default_static = array(
+			'pages',
+			'blocks',
+			'menus',
+			'minerva.pages',
+			'minerva.blocks',
+			'minerva.menus'
+		);
+		$params['minerva_controllers_using_static'] = isset($config['controllers_using_static']) ? $config['controllers_using_static'] += $default_static : $default_static;
 		
-		// Get the library if provided from the route params
-		$library = (isset($params['request']->params['library'])) ? $params['request']->params['library']:'minerva';
-		// is this even necessary?
-	   
-	   
+		
+		// Dependency checks for the CMS
+		$library_deps = array(
+			'li3_flash_message'
+		);
+		
+		$missing_deps = array();
+		foreach($library_deps as $library) {
+			if(Libraries::get($library) == null) {
+				$missing_deps[] = $library;
+			//	throw new Exception('Hey! You don\'t have that library!');
+			}
+		}
+		
+		// TODO: render a template for this
+		if(!empty($missing_deps)) {
+			$view = new View(array('loader' => 'Simple', 'renderer' => 'Simple'));
+			$message =  'You are missing the following libraries that Minerva depends on:<br />';
+			foreach($missing_deps as $library) {
+				$message .= $library . '<br />';
+			}
+			$message .= '<br /><br />You will need to ensure that you have these libraries yourself for now, but in the future there will hopefully be an option to automatically download and install them.';
+			
+			echo $view->render(array('element' => $message));
+			exit();
+		}
+		
+		/**
+		 * The following code will set all the template and layout paths for Minerva.
+		 * 
+		*/
+		
 		// The admin flag from routes helps give control over the templates to use
 		$admin = ((isset($params['request']->params['admin'])) && ($params['request']->params['admin'] == 1 || $params['request']->params['admin'] === true || $params['request']->params['admin'] == 'true' || $params['request']->params['admin'] == 'admin')) ? true:false;
 		
@@ -94,8 +88,7 @@ Dispatcher::applyFilter('_callable', function($self, $params, $chain) {
 		$layout = (isset($params['request']->params['layout'])) ? $params['request']->params['layout']:false;
 		$template = (isset($params['request']->params['template'])) ? $params['request']->params['template']:false;
 		
-		
-		// DEFAULTS
+		// DEFAULT LAYOUT & TEMPLATE PATHS
 		$params['options']['render']['paths']['layout'] = array(
 			'{:library}/views/layouts/{:layout}.{:type}.php'
 		);
@@ -114,16 +107,7 @@ Dispatcher::applyFilter('_callable', function($self, $params, $chain) {
 		 * Special situation; "blocks" and "pages" and "menus" have "static" templates that don't require a datasource.
 		 * This is only the case for the "view" action on these controllers. 
 		*/
-		// TODO: maybe get this from config info set with Libraries::add() ?  (with defaults as listed here)
-		$controllers_with_static_view_methods = array(
-			'pages',
-			'blocks',
-			'menus',
-			'minerva.pages',
-			'minerva.blocks',
-			'minerva.menus'
-		);
-		if(($params['request']->params['action'] == 'view') && (in_array($params['request']->params['controller'], $controllers_with_static_view_methods))) {
+		if(($params['request']->params['action'] == 'view') && (in_array($params['request']->params['controller'], $params['minerva_controllers_using_static']))) {
 			$params['options']['render']['paths']['layout'] = array(
 				'{:library}/views/layouts/static/{:layout}.{:type}.php',
 				'{:library}/views/layouts/{:layout}.{:type}.php'
