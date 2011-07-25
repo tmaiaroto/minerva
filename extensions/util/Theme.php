9d458bc186d4607c85ab134ec3ecc1d57f62a3e5
Tom Maiaroto <tom.maiaroto@gmail.com>
2011-07-25 11:01:41 -0700

    adding theme class

---
+++ extensions/util/Theme.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Minerva: a CMS based on the Lithium PHP framework
+ *
+ * @copyright Copyright 2010-2011, Shift8Creative (http://www.shift8creative.com)
+ * @license http://opensource.org/licenses/bsd-license.php The BSD License
+*/
+namespace minerva\extensions\util;
+
+class Theme extends \lithium\core\StaticObject {
+
+    
+    
+}
+?>
