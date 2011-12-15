1539c92001b7156df8256715ba21095294f2c916
Marc Schwering <mschwering@weluse.de>
2011-12-15 12:44:04 +0100

    config/bootstrap/dependencies fixing whitespaces and some line length violations

--- config/bootstrap/dependencies.php
+++ config/bootstrap/dependencies.php
@@ -4,7 +4,8 @@
  * that includes this file. It will save a filter and some function calls for the system.
  * Besides, once you have all the dependencies for the CMS, this check is completely pointless.
  * It's just to make the CMS a little less rough around the edges, but may become something
- * more robust in the future that may even automatically handle obtaining these dependencies for you.
+ * more robust in the future that may even automatically handle obtaining these dependencies
+ * for you.
  *
 */
 
@@ -19,7 +20,8 @@ Dispatcher::applyFilter('run', function($self, $params, $chain) {
 	if (isset($params['request']->params['library']) && $params['request']->params['library'] == 'minerva') {
 		$config = Libraries::get('minerva');
 
-        // Check for these libraries for the CMS - these can be specified as key => value or just a value if not providing a link.
+        // Check for these libraries for the CMS
+        // these can be specified as key => value or just a value if not providing a link.
 		$library_deps = array(
 			'li3_flash_message' => 'http://dev.lithify.me/li3_flash_message',
             'li3_access' => 'https://github.com/tmaiaroto/li3_access'
@@ -44,11 +46,12 @@ Dispatcher::applyFilter('run', function($self, $params, $chain) {
 
 		if (!empty($missing_deps)) {
 
-            // Borrow the ErrorHanlder's layout and use a new "dependencies" template in the _errors folder.
+            // Borrow the ErrorHanlder's layout and 
+            // use a new "dependencies" template in the _errors folder.
             $view = new View(array(
                 'paths' => array(
                     'template' => '{:library}/views/_errors/{:template}.{:type}.php',
-                    'layout'   => '{:library}/views/layouts/{:layout}.{:type}.php',
+                    'layout'   => '{:library}/views/layouts/{:layout}.{:type}.php'
                 )
             ));
 
