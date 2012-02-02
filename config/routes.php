4b7ed3d0c3c92806666181d7c5e40b6615e4cc02
dgAlien <pti.ms@gmx.net>
2012-02-02 15:24:16 +0100

    refactored routes.php to fit in lithium_qa

--- config/routes.php
+++ config/routes.php
@@ -1,11 +1,11 @@
 <?php
+
 /**
  * Lithium: the most rad php framework
  *
  * @copyright     Copyright 2010, Union of RAD (http://union-of-rad.org)
  * @license       http://opensource.org/licenses/bsd-license.php The BSD License
  */
-
 use lithium\net\http\Router;
 use lithium\core\Environment;
 use lithium\action\Response;
@@ -32,75 +32,116 @@ $admin_prefix = MINERVA_ADMIN_PREFIX;
  * Handles broken URL parsers by matching method URLs with no closing ) and redirecting.
  */
 /*
-Router::connect("{$base}/{:args}\(", array(), function($request) {
-	return new Response(array('location' => "{$request->url})"));
-});
-*/
+  Router::connect("{$base}/{:args}\(", array(), function($request) {
+  return new Response(array('location' => "{$request->url})"));
+  });
+ */
 
 // PLUGIN ROUTES
-Router::connect("{$base}/plugin/{:plugin}/{:admin:$admin_prefix}/{:controller}/{:action}/page:{:page:[0-9]+}/limit:{:limit:[0-9]+}", array(
-    'admin' => $admin_prefix,
-    'library' => 'minerva',
-    'controller' => 'pages'
+$route_basic = "{$base}/plugin/{:plugin}";
+$route_basic_admin = $route_basic . "/{:admin:$admin_prefix}";
+
+$routePatternStrd = "/{:controller}/{:action}";
+$routePatternExt = "/{:controller}/{:action}/{:args}";
+
+$routePatternPaginate = "/page:{:page:[0-9]+}/limit:{:limit:[0-9]+}";
+
+Router::connect($route_basic_admin . $routePatternStrd . $routePatternPaginate, array(
+	'admin' => $admin_prefix,
+	'library' => 'minerva',
+	'controller' => 'pages'
 ));
-Router::connect("{$base}/plugin/{:plugin}/{:admin:$admin_prefix}/{:controller}/{:action}/{:args}/page:{:page:[0-9]+}/limit:{:limit:[0-9]+}", array(
-    'admin' => $admin_prefix,
-    'library' => 'minerva',
-    'controller' => 'pages'
+Router::connect($route_basic_admin . $routePatternExt . $routePatternPaginate, array(
+	'admin' => $admin_prefix,
+	'library' => 'minerva',
+	'controller' => 'pages'
 ));
-Router::connect("{$base}/plugin/{:plugin}/{:admin:$admin_prefix}", array('library' => 'minerva', 'controller' => 'pages', 'action' => 'index'));
-Router::connect("{$base}/plugin/{:plugin}/{:admin:$admin_prefix}/{:controller}/{:action}/{:args}", array('library' => 'minerva'));
+Router::connect($route_basic_admin, array(
+	'library' => 'minerva',
+	'controller' => 'pages',
+	'action' => 'index'
+));
+Router::connect($route_basic_admin . $routePatternExt, array(
+	'library' => 'minerva'
+));
 
 // Non-Admin Plugin Routes
-Router::connect("{$base}/plugin/{:plugin}/{:controller}/{:action}/page:{:page:[0-9]+}/limit:{:limit:[0-9]+}", array(
-    'library' => 'minerva',
-    'controller' => 'pages'
+Router::connect($route_basic . $routePatternStrd . $routePatternPaginate, array(
+	'library' => 'minerva',
+	'controller' => 'pages'
 ));
-Router::connect("{$base}/plugin/{:plugin}/{:controller}/{:action}/{:args}/page:{:page:[0-9]+}/limit:{:limit:[0-9]+}", array(
-    'library' => 'minerva',
-    'controller' => 'dashboards'
+Router::connect($route_basic . $routePatternExt . $routePatternPaginate, array(
+	'library' => 'minerva',
+	'controller' => 'dashboards'
 ));
-Router::connect("{$base}/plugin/{:plugin}", array('library' => 'minerva', 'controller' => 'pages', 'action' => 'view', 'home'));
-Router::connect("{$base}/plugin/{:plugin}/{:controller}/{:action}/{:args}", array('library' => 'minerva'));
+Router::connect($route_basic, array(
+	'library' => 'minerva',
+	'controller' => 'pages',
+	'action' => 'view',
+	'home'
+));
+Router::connect($route_basic . $routePatternExt, array('library' => 'minerva'));
 
 
 // Static Pages
 // first, "/minerva/"
-Router::connect("{$base}", array('library' => 'minerva', 'controller' => 'pages', 'action' => 'view', 'home'));
+Router::connect("{$base}", array(
+	'library' => 'minerva',
+	'controller' => 'pages',
+	'action' => 'view',
+	'home'
+));
 // then "/minerva/page/xxxx"
-Router::connect("{$base}/page/{:args}", array('library' => 'minerva', 'controller' => 'pages', 'action' => 'view'));
+Router::connect("{$base}/page/{:args}", array(
+	'library' => 'minerva',
+	'controller' => 'pages',
+	'action' => 'view'
+));
 // and "/minerva/admin/page/xxxx"
-Router::connect("{$base}/{:admin:$admin_prefix}/page/{:args}", array('admin' => 'admin', 'library' => 'minerva', 'controller' => 'pages', 'action' => 'view'));
+
+$routePatternAdmin = "{$base}/{:admin:$admin_prefix}";
+Router::connect($routePatternAdmin . "/page/{:args}", array(
+	'admin' => 'admin',
+	'library' => 'minerva',
+	'controller' => 'pages',
+	'action' => 'view'
+));
 // and just "/minerva/admin"
-Router::connect("{$base}/{:admin:$admin_prefix}", array('admin' => 'admin', 'library' => 'minerva', 'controller' => 'pages', 'action' => 'view', 'home'));
+Router::connect($routePatternAdmin, array(
+	'admin' => 'admin',
+	'library' => 'minerva',
+	'controller' => 'pages',
+	'action' => 'view',
+	'home'
+));
 
 // Pagination
-Router::connect("{$base}/{:admin:$admin_prefix}/{:controller}/{:action}/page:{:page:[0-9]+}/limit:{:limit:[0-9]+}", array(
-    'library' => 'minerva'
+Router::connect($routePatternAdmin . $routePatternStrd . $routePatternPaginate, array(
+	'library' => 'minerva'
 ));
-Router::connect("{$base}/{:admin:$admin_prefix}/{:controller}/{:action}/{:args}/page:{:page:[0-9]+}/limit:{:limit:[0-9]+}", array(
-    'library' => 'minerva'
+Router::connect($routePatternAdmin . $routePatternExt . $routePatternPaginate, array(
+	'library' => 'minerva'
 ));
 
 // Default Routes
-Router::connect("{$base}/{:admin:$admin_prefix}/{:controller}/{:action}/{:args}", array(
+Router::connect($routePatternAdmin . $routePatternExt, array(
 	'admin' => $admin_prefix,
-    'library' => 'minerva'
+	'library' => 'minerva'
 ));
 
-// Also accept a "url" key; all documents in Minerva have a "url"
-Router::connect("{$base}/{:admin:$admin_prefix}/{:controller}/{:action}/{:url}", array(
-    'library' => 'minerva'
+// Also accept a "url" key; all documents in Minerva have an "url"
+Router::connect($routePatternAdmin . $routePatternStrd . "/{:url}", array(
+	'library' => 'minerva'
 ));
 
 // Public, non-admin default
-Router::connect("{$base}/{:controller}/{:action}/{:args}", array(
-    'library' => 'minerva'
+Router::connect("{$base}" . $routePatternExt, array(
+	'library' => 'minerva'
 ));
 
 // Accept a "url" key here too
-Router::connect("{$base}/{:controller}/{:action}/{:url}", array(
-    'library' => 'minerva'
+Router::connect("{$base}" . $routePatternStrd . "/{:url}", array(
+	'library' => 'minerva'
 ));
 
 ///**
